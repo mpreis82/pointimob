c7ebebd2b6d6098b694b931592793d6091f2a383
mpreis82 <mpreis82@gmail.com>
2022-05-15 05:56:31 -0300

    add plans to new property

--- src/pages/imoveis/novo/publicacao/index.js
+++ src/pages/imoveis/novo/publicacao/index.js
@@ -68,8 +68,6 @@ export default function Publicacao() {
   async function handleSubmit(event) {
     event.preventDefault()
 
-<<<<<<< HEAD
-<<<<<<< HEAD
     try {
       const ref = doc(Firestore, 'properties', localStorage.getItem('new_property_id'))
 
@@ -101,49 +99,6 @@ export default function Publicacao() {
         message: 'Desculpe! Algo deu errado e estamos corrigindo.',
         open: true
       })
-=======
-=======
->>>>>>> 9d88386 (add plans to new property)
-    const propertyId = localStorage.getItem('new_property_id')
-
-    if (propertyId) {
-      try {
-        const ref = doc(Firestore, 'properties', localStorage.getItem('new_property_id'))
-
-        await updateDoc(ref, {
-          publish: {
-            show_property: state.show_property,
-            is_highlighted: state.is_highlighted,
-          },
-          'steps_progress.publish': 'done',
-          'register_status.status': 'done',
-          'register_status.update_date': Timestamp.fromDate(new Date())
-        })
-
-        localStorage.removeItem('new_property_id')
-
-        setAlert({
-          severity: 'success',
-          message: 'Mais um imóvel cadastrado! 🎊🎉',
-          open: true
-        })
-
-        // setTimeout(() => {
-        //   router.push('/imoveis')
-        // }, 6500);
-
-      } catch (err) {
-        console.log(err)
-        setAlert({
-          severity: 'error',
-          message: 'Desculpe! Algo deu errado e estamos corrigindo.',
-          open: true
-        })
-      }
-<<<<<<< HEAD
->>>>>>> 9d88386 (add plans to new property)
-=======
->>>>>>> 9d88386 (add plans to new property)
     }
   }
 
